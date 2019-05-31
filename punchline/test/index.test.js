9b874dbc87da5b0afb5b0d80aa542304078826bb
Vip3rousmango <alexander@virtuallycreative.ca>
2019-05-31 17:24:35 -0400

    Fixed first default test Need to fix Search & Search Test

--- punchline/test/index.test.js
+++ punchline/test/index.test.js
@@ -3,16 +3,25 @@ const cmd = require('..')
 
 describe('punchline', () => {
   test
+  .nock('https://icanhazdadjoke.com', api => api
+  .get('/') // something
+  .reply(200, {joke: 'I hate perforated lines, they\'re tearable.'})
+  )
   .stdout()
   .do(() => cmd.run([]))
-  .it('returns random punchline', ctx => {
-    // expect(ctx.stdout).to.contain('hello world')
+  .it('returns a random punchline', ctx => {
+    expect(ctx.stdout).to.equal('I hate perforated lines, they\'re tearable.\n')
   })
 
-  test
-  .stdout()
-  .do(() => cmd.run(['--search', 'poop']))
-  .it('searches for a punchline --s poop', ctx => {
-    // expect(ctx.stdout).to.contain('hello jeff')
-  })
+  // TODO: Complete Search Feature Testing
+  // test
+  // .nock('https://icanhazdadjoke.com', api => api
+  // .get('/search?query=') // something
+  // .reply(200)
+  // )
+  // .stdout()
+  // .do(() => cmd.run(['--search', 'poop']))
+  // .it('searches for a relevant punchline --s poop', ctx => {
+  //   expect(ctx.stdout).to.equal('I hate perforated lines, they\'re tearable.\n')
+  // })
 })
